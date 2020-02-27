6480961f3a1fa73b997557f5055a0363ad27c9b1
chuong <dangchuongqn91@gmail.com>
2020-02-27 15:04:20 +0700

    test

--- src/PlagiarismCheck.php
+++ src/PlagiarismCheck.php
@@ -4,11 +4,11 @@ namespace Plagiarism\Plagscan;
 use \Plagiarism\Plagscan\Abstracts\AClient;
 
 class PlagiarismCheck extends AClient {
-	public static function submit() {
+	public function submit() {
 		$post_data = array(
 			"access_token"=> "8e7c43e7db7e7e837edb3253b75522859818dae8",
 		);
-		$response = static::$Client->get("users", [ "query" => $post_data]);
+		$response = $this->Client->get("users", [ "query" => $post_data]);
 
 		if($response->getStatusCode() == 200){
 			$content = json_decode($response->getBody(), true);
