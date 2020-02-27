2ad73f06feba53663442eee546fbb030ac1ef00a
chuong <dangchuongqn91@gmail.com>
2020-02-27 14:59:32 +0700

    test

--- src/PlagiarismCheck.php
+++ src/PlagiarismCheck.php
@@ -8,7 +8,7 @@ class PlagiarismCheck extends AClient {
 		$post_data = array(
 			"access_token"=> "8e7c43e7db7e7e837edb3253b75522859818dae8",
 		);
-		$response = $this->Client->get("users", [ "query" => $post_data]);
+		$response = static::$Client->get("users", [ "query" => $post_data]);
 
 		if($response->getStatusCode() == 200){
 			$content = json_decode($response->getBody(), true);
